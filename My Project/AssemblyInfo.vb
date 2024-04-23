4169bfd45db4fbca4eacc3a84e4beca39cf3ede6
asheroto <49938263+asheroto@users.noreply.github.com>
2024-04-23 18:03:25 -0500

    Added Office 2024 radio button

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.1.6.0")>
-<Assembly: AssemblyFileVersion("0.1.6.0")>
+<Assembly: AssemblyVersion("0.1.7.0")>
+<Assembly: AssemblyFileVersion("0.1.7.0")>
